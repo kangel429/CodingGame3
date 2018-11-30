61433bcfffb5b7b55d6b7966e83085bf4dd98a07
hwanungyu <cacao1562@naver.com>
2018-11-30 17:30:34 +0900

    photon  asset add

---
+++ Assets/Photon/Scripts/ConnectDB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ConnectDB : MonoBehaviour {
+
+	public InputField nickField;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	IEnumerator checkNick() {
+
+		WWWForm wWForm = new WWWForm();
+		wWForm.AddField("nick", nickField.text );
+		WWW www = new WWW("http://cacao.dothome.co.kr/dbconn2.php", wWForm );
+		yield return www;
+		Debug.Log("www = " + www.text );
+	}
+	
+
+	public void checkButton() {
+
+		StartCoroutine( checkNick() );
+	}
+
+}
