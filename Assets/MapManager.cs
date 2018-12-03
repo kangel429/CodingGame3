900b9a854eab728cbbba34531ee5ba7d1ce14860
kangel429 <kangel429@nate.com>
2018-12-03 11:50:19 +0900

    kang

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -9,15 +9,20 @@ public class MapManager : MonoBehaviour {
 
     public GameObject parentObj;
 
+    private void Start()
+    {
+        MapVisualize();
+    }
+
 
     // Use this for initialization
     void MapVisualize () {
         for (int x = 0; x < 25; x++)
         {
 
-          //wall[x] = Instantiate(mArrTargetWall[0].gameObject, Vector3.zero, Quaternion.identity).GetComponent<FishInfo>();
-          //wall[x].gameObject.SetActive(false);
-                //wall[x].gameObject.transform.parent = parentObj.transform;
+          wall[x] = Instantiate(mArrTargetWall[0].gameObject, Vector3.zero, Quaternion.identity);
+          wall[x].gameObject.SetActive(false);
+          wall[x].gameObject.transform.parent = parentObj.transform;
 
         
 
